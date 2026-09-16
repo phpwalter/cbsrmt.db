119ed669b39f0b71f11f3042f7902d04056fb42d
Walter <phpwalter@torres.ws>
2026-09-15 21:55:03 -0500

    data: reconcile Gerald Kean writer identity

--- tools/rebuild_data.py
+++ tools/rebuild_data.py
@@ -122,6 +122,7 @@ WRITER_ALIASES = {
     "elspith eric": "245",
     "g frederick louis": "94",
     "murray bur": "114",
+    "gerald kean": "167",
     "roy windsor": "55",
     "roy widnsor": "55",
     "roy windor": "55",
