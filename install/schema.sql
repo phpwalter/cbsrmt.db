f2622af02ecdd6dc79b041f4a4c6a7c585ed00da
Walter <phpwalter@torres.ws>
2026-09-17 21:14:40 -0500

    Add fresh install schema orchestrator

---
+++ install/schema.sql
@@ -0,0 +1,12 @@
+\set ON_ERROR_STOP on
+\echo 'Installing CBS RMT PostgreSQL schema...'
+\ir ../migrations/001_extensions_and_schemas.sql
+\ir ../migrations/002_catalog_tables.sql
+\ir ../migrations/003_relationship_tables.sql
+\ir ../migrations/004_indexes.sql
+\ir ../migrations/005_staging_tables.sql
+\ir ../migrations/006_roles_and_grants.sql
+\ir ../functions/import/promote_json.sql
+\ir ../functions/api/catalog.sql
+\ir ../functions/admin/catalog.sql
+\echo 'CBS RMT PostgreSQL schema installed.'
